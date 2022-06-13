0261e6d8aa15cdc02b9e5a0c5306e3b7380d40be
thayphaphuti <saophaiquay2@gmail.com>
2022-06-13 17:15:45 +0700

    Add project files.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace DotNetTutorial.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome(string name, int id = 1)
+        {
+            ViewData["id"] = id;
+            ViewData["message"] = "Hello  " + name;
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetTutorial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
